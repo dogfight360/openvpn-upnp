2787cdfc89e1636013933c7a9e598608d91e2d94
Alexey Kuznetsov <kuznetsov.alexey@gmail.com>
2010-11-27 18:43:07 +0300

    add first upnp "daemon"

--- openvpn-upnp.py
+++ openvpn-upnp.py
@@ -1,52 +1,72 @@
 #! /usr/bin/python
-# MiniUPnP project
-# Author : Thomas Bernard
-# This Sample code is public domain.
-# website : http://miniupnp.tuxfamily.org/
 
-# import the python miniupnpc module
 import miniupnpc
 import sys
 
-# create the object
-u = miniupnpc.UPnP()
-print 'inital(default) values :'
-print ' discoverdelay', u.discoverdelay
-print ' lanaddr', u.lanaddr
-print ' multicastif', u.multicastif
-print ' minissdpdsocket', u.minissdpdsocket
-u.discoverdelay = 200;
-#u.minissdpdsocket = '../minissdpd/minissdpd.sock'
-# discovery process, it usualy takes several seconds (2 seconds or more)
-print 'Discovering... delay=%ums' % u.discoverdelay
-print u.discover(), 'device(s) detected'
-# select an igd
-try:
-  u.selectigd()
-except Exception, e:
-  print 'Exception :', e
-  sys.exit(1)
-# display information about the IGD and the internet connection
-print 'local ip address :', u.lanaddr
-print 'external ip address :', u.externalipaddress()
-print u.statusinfo(), u.connectiontype()
+from time import sleep
 
-#print u.addportmapping(64000, 'TCP',
-#                       '192.168.1.166', 63000, 'port mapping test', '')
-#print u.deleteportmapping(64000, 'TCP')
+def usage():
+  print '''
+Usage: %s [start] [stop]
 
-port = 0
-proto = 'UDP'
-# list the redirections :
-i = 0
-while True:
-	p = u.getgenericportmapping(i)
-	if p==None:
-		break
-	print i, p
-	(port, proto, (ihost,iport), desc, c, d, e) = p
-	#print port, desc
-	i = i + 1
+Exiting...
+''' % (sys.argv[0])
 
-print u.getspecificportmapping(port, proto)
+def args():
+    if len(sys.argv[1:]):
+        try:
+            (opts, args) = getopt.getopt(sys.argv[1:], '', ['start','stop'])
+            if (len(args)): raise getopt.GetoptError('bad parameter')
 
+        except getopt.GetoptError:
+            usage()
+            sys.exit(0)
+
+        for (opt, arg) in opts:
+            if opt in ('-k', '--key'):
+                key = arg
+            elif opt in ('-s', '--secret'):
+                secret = arg
+
+def show(u):
+  port = 0
+  proto = 'UDP'
+  # list the redirections :
+  i = 0
+  while True:
+    p = u.getgenericportmapping(i)
+    if p==None:
+      break
+    print i, p
+    (port, proto, (ihost,iport), desc, c, d, e) = p
+    #print port, desc
+    i = i + 1
+
+  print u.getspecificportmapping(port, proto)
+
+def start(u):
+  print u.addportmapping(1194, 'TCP', u.lanaddr, 1194, 'OpenVPN-UPNP plugin', '')
+
+def stop(u):
+  print u.deleteportmapping(1194, 'TCP')
+
+def main():
+  u = miniupnpc.UPnP()
+  u.discoverdelay = 200;
+  print u.discover()
+  try:
+    u.selectigd()
+  except Exception, e:
+    print 'Exception :', e
+    sys.exit(1)
+
+  print 'local ip address :', u.lanaddr
+  print 'external ip address :', u.externalipaddress()
+  print u.statusinfo(), u.connectiontype()
+
+  while True:
+    start(u)
+    sleep(60 * 60) # each hour
+
+if __name__ == "__main__":
+    main()
